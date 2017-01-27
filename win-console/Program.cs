afcaf991c692200f4c54b2b81d37715ec6d5027d
Christian Fratta <christianh@learnbrite.com>
2017-01-27 09:41:53 +0100

    Add initial project files for Windows console app

---
+++ win-console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace time_is_ticking_out
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
